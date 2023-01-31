b72ba092aca516e4a1918c69bc6d4c00d8cddec2
lucasuggeri <lucasuggeri@outlook.com>
2023-01-31 11:18:19 -0300

    bot funcionando

--- bot.js
+++ bot.js
@@ -1,11 +1,14 @@
-import { Telegraf } from "telegraf";
 import dotenv from "dotenv";
+import { Telegraf } from "telegraf";
+
 dotenv.config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.command("ping", async (ctx) => {
-  await ctx.telegram.sendMessage(ctx.message.chat.id, "pong");
+bot.command("ping", (ctx) => {
+  ctx.sendMessage("pong");
 });
 
-bot.launch();
+bot.launch().then(() => {
+  console.log("Bot Started!");
+});
