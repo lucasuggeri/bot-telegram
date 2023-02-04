8c2c8a9efc35313b245c240b33a8b54142b4afd9
lucasuggeri <lucasuggeri@outlook.com>
2023-02-03 22:03:54 -0300

    atualização para anotar mensagens recebidas em bloco de notas

--- bot.js
+++ bot.js
@@ -1,14 +1,35 @@
 import dotenv from "dotenv";
+import fs from "fs";
 import { Telegraf } from "telegraf";
+import path from "path";
 
 dotenv.config();
 
+//const now = new Date();
+
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.command("ping", (ctx) => {
-  ctx.sendMessage("pong");
+bot.command("start", async (ctx) => {
+  await ctx.sendMessage(
+    `Olá ${ctx.message.chat.first_name}, digite "/commands" para ver as opções do chat`
+  );
 });
 
-bot.launch().then(() => {
-  console.log("Bot Started!");
+bot.use(async (ctx, next) => {
+  let log = ctx.message.text;
+  let username = ctx.message.chat.first_name;
+  fs.appendFileSync("log.txt", `${username}: ${log}\n`, "utf-8");
+  next();
 });
+bot.on("message", async (ctx) => {
+  await ctx.reply("oi");
+});
+
+bot.launch();
+/*bot.on("message", async (ctx) => {
+  await ctx.sendMessage(
+    `Olá, ${
+      ctx.message.chat.first_name
+    }. Tudo bem? Agora são ${now.getHours()}:${now.getMinutes()}`
+  );
+});*/
