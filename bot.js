260f8c91e9e17efc850c349ab00e8662ddb7eae5
lucasuggeri <lucasuggeri@outlook.com>
2023-02-13 01:04:17 -0300

    bot enviando os eventos da semana todo domingo 00:00

--- bot.js
+++ bot.js
@@ -1,28 +1,29 @@
 import dotenv from "dotenv";
 import fs from "fs";
 import { Telegraf } from "telegraf";
-import path from "path";
+import { authorize, listEvents } from "./index.js";
+import cron from "cron";
 
 dotenv.config();
-
-//const now = new Date();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.command("start", async (ctx) => {
   await ctx.sendMessage(
-    `Olá ${ctx.message.chat.first_name}, digite "/commands" para ver as opções do chat`
+    `Olá ${ctx.message.chat.first_name}, este é um bot para avisar seus eventos do dia`
   );
+  //const weeklyJob = new cron.CronJob("0 0 0 * * 0", async function () {
+  const result = await authorize().then(listEvents).catch(console.error);
+  await ctx.sendMessage(`Seus eventos da semana são:\n${result}`);
 });
+//  weeklyJob.start();
+//});
 
 bot.use(async (ctx, next) => {
   let log = ctx.message.text;
   let username = ctx.message.chat.first_name;
   fs.appendFileSync("log.txt", `${username}: ${log}\n`, "utf-8");
   next();
-});
-bot.on("message", async (ctx) => {
-  await ctx.reply("oi");
 });
 
 bot.launch();
---
+++ index.js
@@ -0,0 +1,108 @@
+import fs from "fs/promises";
+import path from "path";
+import process from "process";
+import { authenticate } from "@google-cloud/local-auth";
+import { google } from "googleapis";
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.cwd(), "token.json");
+const CREDENTIALS_PATH = path.join(process.cwd(), "credentials.json");
+
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client|null>}
+ */
+export async function loadSavedCredentialsIfExist() {
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
+  }
+}
+
+/**
+ * Serializes credentials to a file compatible with GoogleAUth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+export async function saveCredentials(client) {
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: "authorized_user",
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+export async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
+
+/**
+ * Lists the next 10 events on the user's primary calendar.
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+export async function listEvents(auth) {
+  const calendar = google.calendar({ version: "v3", auth });
+  const res = await calendar.events.list({
+    calendarId: "primary",
+    timeMin: new Date().toISOString(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: "startTime",
+  });
+  const now = new Date();
+  const currentDayOfWeek = now.getUTCDay();
+  const daysUntilSunday = 7 - currentDayOfWeek;
+  const nextSunday = new Date(
+    now.getTime() + daysUntilSunday * 24 * 60 * 60 * 1000
+  );
+  const lastSunday = new Date(nextSunday.getTime() - 7 * 24 * 60 * 60 * 1000);
+
+  const events = res.data.items;
+  if (!events || events.length === 0) {
+    return [];
+  }
+
+  const eventData = events
+    .filter((event) => {
+      let start = new Date(event.start.dateTime || event.start.date);
+      return start >= lastSunday && start < nextSunday;
+    })
+    .map((event) => {
+      let start = event.start.dateTime || event.start.date;
+      start = start.replace("T", " / ");
+      return `${start} - ${event.summary}`;
+    });
+
+  return eventData.join("\n");
+}
+
+//authorize().then(listEvents).catch(console.error);
